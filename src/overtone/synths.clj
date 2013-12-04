f7b2eee837f0d68ef5117b673680be14705d0f43
Joseph Wilk <joe@josephwilk.net>
2013-12-04 15:10:49 +0100

    tweaks

--- src/overtone/synths.clj
+++ src/overtone/synths.clj
@@ -6,6 +6,6 @@
   (let [lfo  (+ 0.5 (* 0.5 (sin-osc:kr [(ranged-rand 0.5 1000) (ranged-rand 0.5 1000)] :phase (* 1.5 Math/PI))))
         lfo3 (+ 0.5 (* 0.5 (sin-osc:kr [(ranged-rand 0.1 0.5) (ranged-rand 0.1 0.5)] :phase (* 1.5 Math/PI))))
         lfo2 (+ 0.5 (* 0.5 (sin-osc:kr [(* (ranged-rand 0.5 1000) lfo lfo3) (* (ranged-rand 0.5 1000) (- 1 lfo) (- 1 lfo3))] :phase (* 1.5 Math/PI))))
-        fillers (map (fn [_] (* 0.75 lfo2 (sin-osc:ar (ranged-rand 40 1000) :phase (* 1.5 Math/PI)))) (range 0 100))]
+        fillers (map (fn [_] (* lfo2 (sin-osc:ar (ranged-rand 40 1000) :phase 0))) (range 0 100))]
     (out:ar 0  (* (mix:ar fillers)
                   (env-gen:kr (perc attack decay) :action FREE)))))
