abeb87669e8a0769a853996c9eeec8cd01a614da
Joseph Wilk <joe@josephwilk.net>
2013-12-13 16:32:14 +0100

    always take an out-bus

--- src/overtone/synths.clj
+++ src/overtone/synths.clj
@@ -2,12 +2,12 @@
   (:use overtone.live))
 
 ;; Based on Dawn by Schemawound: http://sccode.org/1-c
-(defsynth fallout-wind [decay 30 attack 30]
+(defsynth fallout-wind [decay 30 attack 30 out-bus 0]
   (let [lfo  (+ 0.5 (* 0.5 (sin-osc:kr [(ranged-rand 0.5 1000) (ranged-rand 0.5 1000)] :phase (* 1.5 Math/PI))))
         lfo3 (+ 0.5 (* 0.5 (sin-osc:kr [(ranged-rand 0.1 0.5) (ranged-rand 0.1 0.5)] :phase (* 1.5 Math/PI))))
         lfo2 (+ 0.5 (* 0.5 (sin-osc:kr [(* (ranged-rand 0.5 1000) lfo lfo3) (* (ranged-rand 0.5 1000) (- 1 lfo) (- 1 lfo3))] :phase (* 1.5 Math/PI))))
         fillers (map (fn [_] (* lfo2 (sin-osc:ar (ranged-rand 40 1000) :phase 0))) (range 0 100))]
-    (out:ar 0  (* (mix:ar fillers)
+    (out:ar out-bus  (* (mix:ar fillers)
                   (env-gen:kr (perc attack decay) :action FREE)))))
 
 (comment
